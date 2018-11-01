bcf776dec6d40e1ac35ed5d55951a00b4f8bc23c
DirupT <iamcooled@gmail.com>
2018-11-01 10:47:55 -0500

    Updates

---
+++ precache-manifest.75f05986a9b9d783119751726ea4b92e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "7df43be885c5c8699ec1",
+    "url": "/Conways-Life/static/js/main.7df43be8.chunk.js"
+  },
+  {
+    "revision": "32ac50fed846cc647243",
+    "url": "/Conways-Life/static/js/1.32ac50fe.chunk.js"
+  },
+  {
+    "revision": "7df43be885c5c8699ec1",
+    "url": "/Conways-Life/static/css/main.3121f344.chunk.css"
+  },
+  {
+    "revision": "864072105b84e3920078d4964ab4faf7",
+    "url": "/Conways-Life/index.html"
+  }
+];
